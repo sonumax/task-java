9c9e5ed6076f1cdc0b025e9322e5f8f3c6decd11
sonumax <sonumax2@gmail.com>
2016-10-21 23:22:23 +0400

    add sorting lines by Argument

--- MyPractic/src/SortInput/SortInput.java
+++ MyPractic/src/SortInput/SortInput.java
@@ -6,8 +6,13 @@ import java.util.stream.Stream;
 
 public class SortInput {
     public static void main(String[] args) {
+        //for sorting by argument
+        if(args.length == 0){
+            System.err.println("No argument");
+            return;
+        }
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        sortArg(reader.lines(), args);
+        sortArg(reader.lines(), args[0]);
     }
 
     private static void sortIgnoreCase(Stream<String> stream){
@@ -20,12 +25,10 @@ public class SortInput {
                 .forEach(System.out::println);
     }
 
-    private static void sortArg(Stream<String> stream, String[] args){
-        if(args.length == 0) {
-            System.err.print("No argument");
-            return;
-        }
-        stream.sorted((first, second) -> )
+    private static void sortArg(Stream<String> stream, String number){
+        Integer numberWord = new Integer(number) - 1;
+        stream.sorted((first, second) -> first.split("[\\W]+")[numberWord].compareToIgnoreCase(second.split("[\\W]+")[numberWord]))
                 .forEach(System.out::println);
+
     }
 }

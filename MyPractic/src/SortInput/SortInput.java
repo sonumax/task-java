dea48b614cefd2a3e870f9d1379613cce96bdcc6
sonumax <sonumax2@gmail.com>
2016-10-24 20:10:55 +0400

    Improvement code in SortInput

--- MyPractic/src/SortInput/SortInput.java
+++ MyPractic/src/SortInput/SortInput.java
@@ -7,27 +7,31 @@ import java.util.stream.Stream;
 public class SortInput {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        sortArg(reader.lines(), args);
+        String[] sortingArray = sortArg(reader.lines(), args);
+
+        for (String line : sortingArray){
+            System.out.println(line);
+        }
     }
 
-    private static void sortIgnoreCase(Stream<String> stream){
-        stream.sorted(String::compareToIgnoreCase)
-                .forEach(System.out::println);
+    private static String[] sortIgnoreCase(Stream<String> stream){
+         return stream.sorted(String::compareToIgnoreCase)
+                .toArray(String[]::new);
     }
 
-    private static void sortLength(Stream<String> stream){
-        stream.sorted((first, second) -> first.length() > second.length() ? 1 : -1)
-                .forEach(System.out::println);
+    private static String[] sortLength(Stream<String> stream){
+        return stream.sorted((first, second) -> first.length() > second.length() ? 1 : -1)
+                .toArray(String[]::new);
     }
 
-    private static void sortArg(Stream<String> stream, String[] strings){
+    private static String[] sortArg(Stream<String> stream, String[] strings){
         if(strings.length == 0){
             System.err.println("No argument");
-            return;
+            return new String[0];
         }
 
         Integer numberWord = new Integer(strings[0]) - 1;
-        stream.sorted((first, second) -> first.split("[\\W]+")[numberWord].compareToIgnoreCase(second.split("[\\W]+")[numberWord]))
-                .forEach(System.out::println);
+        return stream.sorted((first, second) -> first.split("[\\W]+")[numberWord].compareToIgnoreCase(second.split("[\\W]+")[numberWord]))
+                .toArray(String[]::new);
     }
 }

3e924ac00310b4e5401d5b3dd0066adec5533dc6
sonumax <sonumax2@gmail.com>
2016-10-24 19:52:36 +0400

    Improvement code in FilterInput

--- MyPractic/src/SortInput/SortInput.java
+++ MyPractic/src/SortInput/SortInput.java
@@ -6,13 +6,8 @@ import java.util.stream.Stream;
 
 public class SortInput {
     public static void main(String[] args) {
-        //for sorting by argument
-        if(args.length == 0){
-            System.err.println("No argument");
-            return;
-        }
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        sortArg(reader.lines(), args[0]);
+        sortArg(reader.lines(), args);
     }
 
     private static void sortIgnoreCase(Stream<String> stream){
@@ -25,10 +20,14 @@ public class SortInput {
                 .forEach(System.out::println);
     }
 
-    private static void sortArg(Stream<String> stream, String number){
-        Integer numberWord = new Integer(number) - 1;
+    private static void sortArg(Stream<String> stream, String[] strings){
+        if(strings.length == 0){
+            System.err.println("No argument");
+            return;
+        }
+
+        Integer numberWord = new Integer(strings[0]) - 1;
         stream.sorted((first, second) -> first.split("[\\W]+")[numberWord].compareToIgnoreCase(second.split("[\\W]+")[numberWord]))
                 .forEach(System.out::println);
-
     }
 }
